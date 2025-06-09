b543510b6aeedf802463050ea4a88ad93677008a
Nicholas <cantoagi@gmail.com>
2025-06-09 08:57:06 +0800

    adding more description on about us page

--- src/data/aboutUsText.ts
+++ src/data/aboutUsText.ts
@@ -2,7 +2,9 @@ const aboutUsText = {
   title: "Bennett",
   description: [
     "Bennett is an AI assistant that handles your accounting chores, freeing you to focus on what truly matters.",
-    "Just as AI was originally created to bridge the gap between humans and countless tasks, Bennett’s mission is to make finance and accounting accessible and effortless for everyone."
+    "Just as AI was originally created to bridge the gap between humans and countless tasks, Bennett’s mission is to make finance and accounting accessible and effortless for everyone.",
+        "We use AI for invoice extraction, transaction classification, and bank record reconciliation to streamline your financial workflows."
+
   ],
 };
 
