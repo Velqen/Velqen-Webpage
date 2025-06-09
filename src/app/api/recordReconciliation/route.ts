2f02ab26f065cc3b6df7cd6eaf8a024694ba83e7
Nicholas <cantoagi@gmail.com>
2025-06-09 08:42:28 +0800

    adding record reconciler backend

--- src/app/api/recordReconciliation/route.ts
+++ src/app/api/recordReconciliation/route.ts
@@ -1,10 +1,12 @@
 // app/api/recordReconciliation/route.ts
 import { NextRequest } from "next/server";
 
+const RECORD_RECONCILER_URL = process.env.RECORD_RECONCILER_URL
+
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
 
-  const res = await fetch("http://localhost:5003/record-reconciler", {
+  const res = await fetch(`${RECORD_RECONCILER_URL}/record-reconciler`, {
     method: "POST",
     body: formData,
   });
