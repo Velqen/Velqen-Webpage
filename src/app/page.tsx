c8a5b7b431c41b78b478f09975bd6c76c27d8cd6
Nicholas <cantoagi@gmail.com>
2025-06-19 12:26:21 +0800

    home colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,9 @@ export default function Home() {
   return isSmallDevice ? (
     <div className="min-h-screen w-full flex flex-col justify-center items-center pt-24">
       <div className="w-[90%] flex flex-col justify-center items-center">
-        <h1 className="text-4xl py-4 ">Pick Your AI Tool</h1>
+        <h1 className="text-4xl py-4 font-bold">
+          Pick Your <span className="bennett-gradient-text">AI</span> Tool
+        </h1>
         <GlideLink />
       </div>
 
@@ -81,7 +83,9 @@ export default function Home() {
   ) : (
     <div className="min-h-screen w-full flex flex-col justify-center items-center pt-44">
       <div className="w-[80%] flex flex-col justify-center items-center">
-        <h1 className="text-7xl py-4 ">Pick Your AI Tool</h1>
+        <h1 className="text-7xl py-4 font-bold">
+          Pick Your <span className="bennett-gradient-text">AI</span> Tool
+        </h1>
         <GlideLink />
       </div>
 
